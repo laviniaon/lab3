fdc784a7897659857335135afadb91291ae2b0c1
laviniaon <laviniaoliveiranascimento1@gmail.com>
2026-06-14 20:43:45 -0300

    Delete src/lab3q1 directory

--- src/lab3q1/Ave.java
+++
@@ -1,6 +0,0 @@
-package lab3q1;
-
-public interface Ave {
-	   void voar();
-	   void emitirSom();	
-}
--- src/lab3q1/PavaoAzul.java
+++
@@ -1,7 +0,0 @@
-package lab3q1;
-
-public class PavaoAzul {
-    public void cantar() {
-        System.out.println("Pavão Azul emitindo um canto estridente!");
-    }
-}
